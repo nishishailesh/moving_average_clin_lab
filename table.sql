c8ee448ca16b0d3845c42cafa070dafd307594dc
Shaileshkumar Manubhai Patel <nishishailesh@gmail.com>
2021-12-14 17:29:05 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,5 +1,8 @@
- CREATE TABLE `moving_average` (
+CREATE TABLE `moving_average` (
   `examination_id` int(11) NOT NULL,
   `date_time` datetime NOT NULL,
-  `avg_value` decimal(10,2) NOT NULL,
+  `avg_value` decimal(10,2) DEFAULT NULL,
+  `sample_id` bigint(20) DEFAULT NULL,
+  `value` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`examination_id`,`date_time`)
+) 
